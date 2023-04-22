da6a6ffff7777683bafd38aab2063cf631eddb6f
KKTECHIS0211 <makisechris0725@gmail.com>
2023-04-22 13:39:45 +0900

    ログイン機能

--- Views/home.php
+++ Views/home.php
@@ -1,50 +1,15 @@
-<?php
-// 設定関連を読み込む
-include_once('/Applications/MAMP/htdocs/TwitterClone/config.php');
-// 便利な関数を読み込む
-include_once('/Applications/MAMP/htdocs/TwitterClone/Views/util.php');
- 
-///////////////////////////////////////
-// ツイート一覧
-///////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-03-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
- 
-?>
 <!DOCTYPE html>
 <html lang="ja">
  
 <head>
-    <?php include_once('../Views/common/head.php'); ?>
+    <?php include_once('/Applications/MAMP/htdocs/TwitterClone/Views/common/head.php'); ?>
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
 </head>
  
 <body class="home">
     <div class="container">
-        <?php include_once('../Views/common/side.php'); ?>
+        <?php include_once('/Applications/MAMP/htdocs/TwitterClone/Views/common/side.php'); ?>
         <div class="main">
             <div class="main-header">
                 <h1>ホーム</h1>
@@ -77,13 +42,13 @@ $view_tweets = [
             <?php else : ?>
                 <div class="tweet-list">
                     <?php foreach ($view_tweets as $view_tweet) : ?>
-                        <?php include('../Views/common/tweet.php'); ?>
+                        <?php include('/Applications/MAMP/htdocs/TwitterClone/Views/common/tweet.php'); ?>
                     <?php endforeach; ?>
                 </div>
             <?php endif; ?>
         </div>
     </div>
-    <?php include_once('../Views/common/foot.php'); ?>
+    <?php include_once('/Applications/MAMP/htdocs/TwitterClone/Views/common/foot.php'); ?>
 </body>
  
 </html>
